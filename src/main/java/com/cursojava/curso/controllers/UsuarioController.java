17584f7f479c67d1fa9dc1e598cb403c625960d9
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-08-18 17:13:41 -0500

    Creando modelo de usuario e invocandolo en peticion get desde UsuarioController

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,12 +1,22 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public String prueba() {
-        return "Prueba";
+    @RequestMapping(value = "usuario")
+    public Usuario getUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Cristian");
+        usuario.setApellido("Gutierrez");
+        usuario.setEmail("cristian@gmail.com");
+        usuario.setTelefono("3232332323");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario")
+    public Usuario editar() {
+
     }
 }
